527c4229ade5a38d3ed710c8088e3e35bc5521c5
frkn8 <103068163+frkn8@users.noreply.github.com>
2023-03-29 00:55:48 +0300

    başlandı

--- src/App.js
+++ src/App.js
@@ -5,17 +5,25 @@
 */
 
 // State hook u import edin
-import React from 'react';
-
+import React, {useState} from 'react';
+import "./App.css";
+import AramaÇubuğu from "./bileşenler/AramaÇubuğu/AramaÇubuğu.js";
+import Gönderiler from "./bileşenler/Gönderiler/Gönderiler.js";
+import Yorumlar from "./bileşenler/Yorumlar/Yorumlar.js";
+import sahteVeri from "./sahte-veri.js";
 // Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
 // sahteVeri'yi import edin
 import './App.css';
+
 
 const App = () => {
   // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
   // Artık sahteVeri'ye ihtiyacınız olmayacak.
   // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
 	
+	const [gonderiler, setGonderiler] = useState(sahteVeri);
+	const [arama, setArama] = useState("");
+
   const gonderiyiBegen = gonderiID => {
     /*
       Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
@@ -28,6 +36,17 @@ const App = () => {
         - gönderinin idsi "gonderiID" ile eşleşirse, istenen değerlerle yeni bir gönderi nesnesi döndürün.
         - aksi takdirde, sadece gönderi nesnesini değiştirmeden döndürün.
      */
+        const [gonderiler, setGonderiler] = useState(sahteVeri);
+        const [arama, setArama] = useState("");
+      
+        const gonderiyiBegen = (gonderiID) => {
+          setGonderiler(
+            sahteVeri.map((post) => {
+              return post.id === gonderiID ? (post.likes = post.likes +1) : post;
+            })
+          );
+        };
+
   };
 
   return (
